fe6c115614d5c99992a8ba4b89809024539b3acb
Alex-Programer <alex.lichangnan@gmail.com>
2023-11-01 11:55:29 +0800

    feat: config nextui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@nextui-org/react";
+
 function App() {
   return (
     <div className="container">
       <h1 className="font-bold">Welcome to Tauri!</h1>
+      <Button color="primary" variant="shadow">
+        Button
+      </Button>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
+import { NextUIProvider } from "@nextui-org/react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <NextUIProvider>
+    <App />
+  </NextUIProvider>
+);
