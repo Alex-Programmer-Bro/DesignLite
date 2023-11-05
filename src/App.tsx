0d4ba5d4d99d8969ba4abe0ce241568ba81f075c
biubiujch <coolechi@foxmail.com>
2023-11-05 11:40:17 +0800

    feat: toolsbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Canvas } from "./component/canvas";
 import { Designer } from "./component/designer";
-import { Toolbar } from './component/toolbar';
+import { Toolsbar } from "./component/toolsbar";
 
 function App() {
   return (
     <div className="flex h-full">
       <div className="h-full flex-1 border-r-1 border-solid border-[#ddd]">
-        <Toolbar />
+        <Toolsbar />
         <Canvas />
       </div>
       <Designer />
---
+++ src/component/toolsbar/index.tv.ts
@@ -0,0 +1,8 @@
+import { tv } from "tailwind-variants";
+
+export const styles = tv({
+  slots: {
+    wrap: "px-8 flex items-center text-white bg-black",
+    drop: "flex items-center gap-4"
+  }
+});
