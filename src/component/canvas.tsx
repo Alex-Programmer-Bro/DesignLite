2dca3aca1b4fc802c5f2340939bdcd562debed29
janchain <chrislee4642@gmail.com>
2023-11-06 17:33:03 +0800

    update: canvas style

--- src/component/canvas.tsx
+++ src/component/canvas.tsx
@@ -5,9 +5,13 @@ import { SchemaRender } from "./schemaRender";
 export const Canvas = () => {
   const schemas = useAtomValue(schemasAtom);
 
-  return <div>
-    {
-      schemas.map(item => <SchemaRender key={item.id} {...item} />)
-    }
-  </div>
+  return (
+    <div className="w-[calc(100vw-300px)] flex-1 overflow-auto box-border relative ">
+      <div className="shadow-medium  bg-white m-16">
+        {schemas.map((item) => (
+          <SchemaRender key={item.id} {...item} />
+        ))}
+      </div>
+    </div>
+  );
 };
