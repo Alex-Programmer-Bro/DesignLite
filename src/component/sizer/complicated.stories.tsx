db1527b4040d1cd9ef7002cade99519dc8b3733b
Alex-Programer <alex.lichangnan@gmail.com>
2023-11-07 11:31:48 +0800

    feat: state

--- src/component/sizer/complicated.stories.tsx
+++ src/component/sizer/complicated.stories.tsx
@@ -1,6 +1,7 @@
+import { expect } from "@storybook/jest";
+import { useState } from '@storybook/preview-api';
 import { StoryObj, type Meta } from "@storybook/react";
 import { ComplicatedSizer } from "./complicated";
-import { useState } from '@storybook/preview-api';
 import { Value } from "./type";
 
 const meta = {
@@ -32,6 +33,30 @@ export const Primary: Story = {
       });
     },
   ],
+  play: async ({ canvasElement, step }) => {
+    await new Promise(resolve => setTimeout(resolve, 300));
+    const switchTarget = canvasElement.querySelector('input[type="checkbox"]') as HTMLInputElement;
+
+    await step('default ui', () => {
+      const inputs = canvasElement.querySelectorAll('input');
+      expect(inputs.length).toEqual(2);
+      const singleInput = canvasElement.querySelector('input[type="number"]') as HTMLInputElement;
+      expect(singleInput.value).toEqual('10');
+    })
+
+    await step('switch status', () => {
+      expect(switchTarget.checked).toBeTruthy();
+      switchTarget.click();
+    })
+
+    await step('keep a same value while first toggle', () => {
+      const switchedInputs = canvasElement.querySelectorAll('input[type="number"]')
+      expect(switchedInputs.length).toEqual(4);
+      expect(switchTarget.checked).toBeFalsy();
+      const values = ([...switchedInputs] as HTMLInputElement[]).map(item => item.value);
+      expect(values).toEqual(['10', '10', '10', '10']);
+    })
+  }
 }
 
 export const ComplicatedValue: Story = {
@@ -42,7 +67,6 @@ export const ComplicatedValue: Story = {
   decorators: [
     (story, config) => {
       const [value, setValue] = useState<Value>(config.args.value);
-      console.log(value);
       return story({
         args: {
           ...config.args,
