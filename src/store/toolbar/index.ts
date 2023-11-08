d544cc56850482ca22c0a64d730da3b14c8b19a7
janchain <chrislee4642@gmail.com>
2023-11-08 18:45:39 +0800

    fix: 取消选择后，选中schema未清除

--- src/store/toolbar/index.ts
+++ src/store/toolbar/index.ts
@@ -1,8 +1,22 @@
 import { atom } from "jotai";
+import { appStore } from "..";
 import { SchemaType } from "../../types/schema";
+import { drawingSchemaIdAtom } from "../schema";
 
 export const selectedDrawTypeAtom = atom<SchemaType>(SchemaType.Text);
 selectedDrawTypeAtom.debugLabel = "准备添加的 Schema 类型";
 
 export const allowSelectAtom = atom<boolean>(false);
 selectedDrawTypeAtom.debugLabel = "是否允许选择 Schema";
+
+appStore.sub(allowSelectAtom, () => {
+  const allowSelect = appStore.get(allowSelectAtom);
+  if (!allowSelect) {
+    appStore.set(drawingSchemaIdAtom, "");
+    
+    const elements = document.getElementsByClassName("schema-active");
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.remove("schema-active");
+    }
+  }
+});
