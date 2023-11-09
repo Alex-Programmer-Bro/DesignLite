0ccb8128e9b23702ad830047ce04eea28805f0eb
Alex-Programer <alex.lichangnan@gmail.com>
2023-11-09 13:16:53 +0800

    chore: debug label

--- src/store/toolbar/index.ts
+++ src/store/toolbar/index.ts
@@ -7,7 +7,7 @@ export const selectedDrawTypeAtom = atom<SchemaType>(SchemaType.Text);
 selectedDrawTypeAtom.debugLabel = "准备添加的 Schema 类型";
 
 export const allowSelectAtom = atom<boolean>(false);
-selectedDrawTypeAtom.debugLabel = "是否允许选择 Schema";
+allowSelectAtom.debugLabel = "是否允许选择 Schema";
 
 appStore.sub(allowSelectAtom, () => {
   const allowSelect = appStore.get(allowSelectAtom);
