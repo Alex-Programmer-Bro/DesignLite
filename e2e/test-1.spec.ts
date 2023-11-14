2c08846a029a74e5b55403f2ed60c66aa3c46f40
Alex-Programer <alex.lichangnan@gmail.com>
2023-11-14 08:01:17 +0800

    chore: get download path

--- e2e/test-1.spec.ts
+++ e2e/test-1.spec.ts
@@ -1,12 +1,17 @@
 import { test } from '@playwright/test';
-import { resolve } from 'path';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:1420/');
   await page.getByRole('button', { name: '操作' }).click();
-  await page.getByText('使用模版代码内预制的模版').click();
+  await page.getByText('使用模版').click();
   const downloadPromise = page.waitForEvent('download');
-  await page.getByText('导出资源导出前端静态资源，以供部署上线').click();
+  await page.getByText('导出资源').click();
   const download = await downloadPromise;
-  await download.saveAs(resolve(__dirname, download.suggestedFilename()));
+  const downloadPath = resolve(__dirname, download.suggestedFilename());
+  await download.saveAs(downloadPath);
 });
