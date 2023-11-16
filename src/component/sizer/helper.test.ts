ed52ac01699107b6c7e51646082006684fd6e127
Alex <115539090+Alex-Programmer-Bro@users.noreply.github.com>
2023-11-17 05:42:39 +0800

    test: helper test coverage

--- src/component/sizer/helper.test.ts
+++ src/component/sizer/helper.test.ts
@@ -2,6 +2,7 @@ import { expect, test } from 'vitest';
 import { resolveLock, resolveUnit, resolveValue } from './helper';
 
 test('resolveLock', () => {
+  expect(resolveLock('')).toBeTruthy();
   expect(resolveLock('1px 1px 1px 1px')).toBeTruthy();
   expect(resolveLock('1px 2px 1px 1px')).toBeFalsy();
   expect(resolveLock('auto')).toBeTruthy();
@@ -13,6 +14,7 @@ test('resolveUnit', () => {
   expect(resolveUnit('1%')).toEqual('%');
   expect(resolveUnit('auto')).toEqual('auto');
   expect(resolveUnit('auto auto')).toEqual('auto');
+  expect(() => resolveUnit('invalid')).toThrowErrorMatchingInlineSnapshot('"unknow unit of invalid"');
 });
 
 test('resolveValue', () => {
@@ -20,4 +22,5 @@ test('resolveValue', () => {
   expect(resolveValue('10px 20px')).toEqual(['10px', '20px', '10px', '20px']);
   expect(resolveValue('1%')).toEqual('1');
   expect(resolveValue('auto')).toEqual('');
+  expect(resolveValue('10px 10px 10px 10px')).toEqual('10px 10px 10px 10px');
 });
