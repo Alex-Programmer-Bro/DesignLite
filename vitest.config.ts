dd1e78770736d30a757106830f69a8cbba9c73a8
Alex-Programer <alex.lichangnan@gmail.com>
2023-11-17 15:23:15 +0800

    chore: test

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      lines: 100,
+      functions: 100,
+      branches: 100,
+      statements: 100
+    },
     globals:true,
     include: ['src/**/*.test.{ts,tsx}'],
     environment: 'jsdom',
