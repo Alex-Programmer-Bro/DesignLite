4bc272fd53c0c32ebdc10e1921ad72414f237ceb
Alex-Programer <alex.lichangnan@gmail.com>
2023-11-22 10:21:57 +0800

    doc: change log

--- ChangeLog.h
+++ ChangeLog.h
@@ -1,3 +1,3 @@
-Release 0.0.2-alpla:
-  - 图文编辑强化
-  - 提高功能流程稳定性
+Release 0.0.3-alpla:
+  - Toolsbar 布局优化
+  - 画布功能支持
--- ChangeLogHistory.h
+++ ChangeLogHistory.h
@@ -1,3 +1,7 @@
+Release 0.0.3-alpla:
+  - Toolsbar 布局优化
+  - 画布功能支持
+
 Release 0.0.2-alpla:
   - 图文编辑强化
   - 提高功能流程稳定性
