381dde241ad0c206f5f8622ae1c71b2cba2a8262
Alex-Programer <alex.lichangnan@gmail.com>
2023-11-29 17:47:29 +0800

    release

--- ChangeLog.h
+++ ChangeLog.h
@@ -1,5 +1,5 @@
-Release 0.0.3-alpla:
-  - Toolsbar 布局优化
-  - 画布功能支持
-  - 修复画布元素和样式编辑器的数据统一问题
-  - 确定项目 Logo
+Release 0.0.4-alpla:
+  - 画布功能强化，支持缩放
+  - 选中画布的外框跟随变化
+  - 页面控制器支持
+  - 分离 Desinger 为 Layout 和 Meta
