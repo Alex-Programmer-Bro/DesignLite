31acc320a1c60f4cbf428d9e1188ff95cd6c05a1
janchain <chrislee4642@gmail.com>
2023-12-08 10:32:36 +0800

    update: preview schema editable

--- src/component/schemaEditor/index.test.tsx
+++ src/component/schemaEditor/index.test.tsx
@@ -1,23 +1,3 @@
-import { render, screen } from '@testing-library/react';
-import { describe, it } from 'vitest';
-import { SchemaEditor } from '.';
+import { describe } from 'vitest';
 
-describe('complicated sizer', () => {
-  it('should be null', () => {
-    render(<SchemaEditor />);
-    expect(screen.queryByLabelText('schema-mask')).toBeNull();
-  });
-
-  it('should be null while id is not exist', () => {
-    const App = () => {
-      return (
-        <div>
-          <div id='hello'>123</div>
-          <SchemaEditor id='world' />
-        </div>
-      );
-    };
-    render(<App />);
-    expect(screen.queryByLabelText('schema-mask')).toBeNull();
-  });
-});
+describe('complicated sizer', () => {});
