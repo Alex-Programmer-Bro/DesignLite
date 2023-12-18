87b5b11b777036b4371d3d944675ba6c597cc053
Alex-Programer <alex.lichangnan@gmail.com>
2023-12-18 18:33:41 +0800

    chore: clean fold

--- ChangeLog.h
+++ ChangeLog.h
@@ -1,7 +1,2 @@
-Release 0.0.4-alpla:
-  - 分解了 Designer 为 Layout 和 Meta 管理 schema 样式与内容
-  - 缩放中心点调整
-  - 快捷键拖拽画布
-  - 修复了app端不能编辑问题
-  - 修复了活动 schema 样式适应问题
-  - 修复不能设置透明度的问题
+Release 0.0.5-alpla:
+  - 支持 schema 编辑
